71afa2e55a7bcc2587f40516946317c849996fbc
ahaoboy <504595380@qq.com>
2025-03-21 18:43:52 +0800

    ci: key

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -23,6 +23,7 @@ use tauri::menu::{Menu, MenuItem};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_positioner::init())
         .setup(|app| {
             let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
